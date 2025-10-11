fd15d2be8ae1c18c2e7becdaa4982fe87b6d8828
Niyushii <1302nicolzelaya@gmail.com>
2025-10-11 17:39:10 -0400

    categorias modelo

--- apps/categorias/models.py
+++ apps/categorias/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    categoriaID = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    icono = models.CharField(max_length=255, blank=True, null=True)
+    categoriaPadre = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='subcategorias'
+    )
+    estado = models.CharField(
+        max_length=20,
+        default='activo',
+    )
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    fechaEliminacion = models.DateTimeField(null=True, blank=True)
+    fechaModificacion = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'categoria'
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+        ordering = ['nombre']
+
+    def __str__(self):
+        return self.nombre
